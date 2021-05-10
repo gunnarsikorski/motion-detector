05268fdd71b772063541ca6f8b0dbe6405441022
gunnarsikorski <gunnarsikorski@gmail.com>
2021-05-10 15:17:33 -0500

    Added rectangle to pick up motion

--- app.py
+++ app.py
@@ -15,12 +15,21 @@ while True:
         continue
     
     delta_frame = cv2.absdiff(first_frame, gray) # Shows difference between frist_frame and gray
-
     thresh_frame = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
-
     thresh_frame = cv2.dilate(thresh_frame, None, iterations = 2)
 
+    (cnts,_) = cv2.findcon(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        
     cv2.imshow('Capturing', gray)
+    cv2.imshow('Delta Frame', delta_frame)
+    cv2.imshow('Threshold Frame', thresh_frame)
+    cv2.imshow('Color Frame', frame)
 
     key = cv2.waitKey(1)
     print(gray)
