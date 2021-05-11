e87180aeb1aefe41301fb79eb427a4e9ae5dfb63
gunnarsikorski <gunnarsikorski@gmail.com>
2021-05-11 13:59:45 -0500

    Adding time stamps for when motion is detected in frame

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import cv2, time
+from datetime import datetime
 
 first_frame = None
+status_list = []
+times = []
 
 video = cv2.VideoCapture(0)
 
 while True:
     check, frame = video.read()
-
+    status = 0
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -23,19 +26,28 @@ while True:
     for contour in cnts:
         if cv2.contourArea(contour) < 1000:
             continue
+        status = 1
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
-        
+    
+    status_list.append(status)
+
+    if status_list[-1] == 1 and status_list[-2] == 0: 
+        times.append(datetime.now())
+    if status_list[-1] == 0 and status_list[-2] == 1: 
+        times.append(datetime.now())
+
+
     cv2.imshow('Capturing', gray)
     cv2.imshow('Delta Frame', delta_frame)
     cv2.imshow('Threshold Frame', thresh_frame)
     cv2.imshow('Color Frame', frame)
 
     key = cv2.waitKey(1)
-    print(gray)
 
     if key == ord('q'):
         break
+
 
 video.release()
 cv2.destroyAllWindows()
